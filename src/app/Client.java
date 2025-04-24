f22ad3434ef7c90a989cce344c2d2fc1e4f1210f
VladislavSCH <114906748+VladislavSCH@users.noreply.github.com>
2025-04-24 12:14:15 +0300

    Creating app Using The Factory Method

---
+++ src/app/Client.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Client {
+    public void run(){
+        TransportFactory carFactory = new CarFactory();
+        Transport car = carFactory.createTransport();
+        car.move();
+
+        TransportFactory planeFactory = new PlaneFactory();
+        Transport plane = planeFactory.createTransport();
+        plane.move();
+    }
+}
